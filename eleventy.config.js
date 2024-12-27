9d6301860b009ca8813bd147ec05e757fc158747
abhi19751 <153172902+abhi19751@users.noreply.github.com>
2024-12-27 19:25:21 +0530

    Update eleventy.config.js

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   return {
     dir: {
       input: "content",
-      output: "_site",
+      output: "_my-website",
       includes: "../templates"
     }
   };
